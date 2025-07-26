c3d8da46f2d1fea4955b24f8766e20b46d5472bf
Kamida SYOU <e245709@eve.u-ryukyu.ac.jp>
2025-07-26 11:43:19 +0900

    Rename model to database, restructure files and add new database-related files

---
+++ database/models/fuction.py
@@ -0,0 +1,12 @@
+from db.database import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+
+    posts = db.relationship('Post', back_populates='user', cascade='all, delete-orphan')
+
+    def __repr__(self):
+        return f'<User {self.username}>'
